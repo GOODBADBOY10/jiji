237e95ea9f49f96c83b99a1e6c01f85bf2e82570
ademola-adebisi <ademolaidris2002@gmail.com>
2024-03-22 15:01:53 +0100

    Added some code in app.jsx

--- buildhubb/src/App.jsx
+++ buildhubb/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login'
+import ForgetPassword from './components/ForgetPassword'
+import VerifyPassword from './components/VerifyPassword'
+import ResetPassword from './components/ResetPassword'
+import ResetSuccess from './components/ResetSuccess'
 
 function App() {
-
   return (
     <>
-     
+    <Login />
+    {/* <ForgetPassword />
+    <VerifyPassword />
+    <ResetPassword />
+    <ResetSuccess /> */}
     </>
   )
 }
